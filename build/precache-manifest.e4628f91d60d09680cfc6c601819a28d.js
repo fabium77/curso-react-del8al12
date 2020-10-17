85704e979173488c7da1a0fa4f8250078067ccb4
fabium77 <fabium77@gmail.com>
2020-10-17 05:32:34 -0300

    Preparando para subir

---
+++ build/precache-manifest.e4628f91d60d09680cfc6c601819a28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b11e603d5ae9a1486edd704224f078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d40854a47962f39ce0",
+    "url": "/static/css/main.f2be1d4d.chunk.css"
+  },
+  {
+    "revision": "6f219954e688298448c8",
+    "url": "/static/js/2.8114014d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8114014d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d40854a47962f39ce0",
+    "url": "/static/js/main.6adcc859.chunk.js"
+  },
+  {
+    "revision": "425dd922c8c64b58070a",
+    "url": "/static/js/runtime-main.94cb6c4c.js"
+  }
+]);
